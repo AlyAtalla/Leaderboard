df8074a388d61eac997abfc43426d09aa1e040c4
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-07-24 12:33:04 +0300

    Add .gitignore

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,5 @@
 import "../styles.css";
 
-// Function to generate random scores (for demonstration purposes).
 const generateRandomScores = () => {
   const names = ["John Doe", "Jane Smith", "Mike Johnson", "Alice Lee", "Bob Brown"];
   const scores = Array.from({ length: names.length }, () => Math.floor(Math.random() * 100) + 1);
@@ -8,7 +7,6 @@ const generateRandomScores = () => {
   return names.map((name, index) => ({ name, score: scores[index] }));
 };
 
-// Function to populate the table with random scores.
 const populateTable = () => {
   const data = generateRandomScores();
 
@@ -25,17 +23,14 @@ const populateTable = () => {
   });
 };
 
-// Event listener for the refresh button.
 document.querySelector(".refresh-btn").addEventListener("click", () => {
   populateTable();
 });
 
-// Event listener for the submit button (placeholder for future functionality).
 document.querySelector(".submit-btn").addEventListener("click", () => {
   const name = document.querySelector(".input-name").value;
   const score = document.querySelector(".input-score").value;
   console.log("Name:", name, "Score:", score);
 });
 
-// Initial table population on page load.
 populateTable();
